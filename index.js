736fbe3f302a9f144044e312dc7feb4c25506fcc
Ngai Tik Chung <NgaitikK.Chung94@gmail.com>
2023-08-31 12:51:18 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = 'ABC';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+
+let currentUser1 = currentUser[0]
+let shortGreeting = `Welcome, ${currentUser1}!`;
